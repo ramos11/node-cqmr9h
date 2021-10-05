e75103ff1093adfa4ed039b0a3dc702b3e304adc
ramos11 <ramsie1@gmail.com>
2021-10-05 14:22:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-cqmr9h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-cqmr9h)
---
+++ index.js
@@ -0,0 +1,10 @@
+// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
+
+var x = 10 + 2 * 15;
+
+var y = (10 + 2) * 15;
+
+console.log(x, y);
+
